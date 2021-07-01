fbdcf745b437ab77eb23cd6fab6a5a906561fbac
Vinay Venu <ijkvinay@gmail.com>
2021-07-01 16:53:25 +0530

    Replace select with update script

--- data migrations/FD474-2020-07-01.sql
+++ data migrations/FD474-2020-07-01.sql
@@ -1,3 +1,5 @@
 -- Update audits for the two rows that are in translations
 set role sncu;
-select * from audit where id in (select audit_id from translation);
+update audit
+set last_modified_date_time = now()
+where id in (select audit_id from translation);
